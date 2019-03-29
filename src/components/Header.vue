8d99a52a1a5fe40e8609d1872b79f65965fe74ca
sakura <929094868@qq.com>
2019-03-29 18:34:35 +0800

    config

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+
+  </header>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped lang="scss">
+header{
+  width: 100%;
+  height: 70px;
+  background: #242f42;
+  box-sizing: border-box;
+  }
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="sakura">
+    <Header></Header>
+
+  </div>
+</template>
+
+<script>
+import Header from './Header'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    Header,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>

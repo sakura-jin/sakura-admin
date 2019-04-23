fefc42a138c0048648ce4250d45680d64cccb404
sakura <929094868@qq.com>
2019-04-23 18:32:17 +0800

    config

--- src/components/DragList.vue
+++ src/components/DragList.vue
@@ -10,16 +10,44 @@
 
            <div class="drag-box-item">
              <div class="item-title">todo</div>
-
+             <draggable v-model="todo" v-bind="dragOptions">
+               <transition-group tag="div" id="todo" class="item-ul">
+                 <div v-for="item in todo" class="drag-list" :key="item.id">
+                   {{item.content}}
+                 </div>
+               </transition-group>
+             </draggable>
            </div>
 
+          <div class="drag-box-item">
+            <div class="item-title">doing</div>
+            <draggable v-model="doing" v-bind="dragOptions">
+              <transition-group tag="div" id="doing" class="item-ul">
+                <div v-for="item in doing" class="drag-list" :key="item.id">
+                  {{item.content}}
+                </div>
+              </transition-group>
+            </draggable>
+          </div>
+
+          <div class="drag-box-item">
+            <div class="item-title">done</div>
+            <draggable v-model="done" v-bind="dragOptions">
+              <transition-group tag="div" id="done" class="item-ul">
+                <div v-for="item in done" class="drag-list" :key="item.id">
+                  {{item.content}}
+                </div>
+              </transition-group>
+            </draggable>
+          </div>
         </div>
 
       </div>
     </div>
 </template>
-
 <script>
+
+    import draggable from 'vuedraggable'
     export default {
         name: "DragList",
         data(){
@@ -33,47 +61,50 @@
             todo: [
               {
                 id: 1,
-                content: '开发图表组件'
+                content: '火影忍者'
               },
               {
                 id: 2,
-                content: '开发拖拽组件'
+                content: '东京食尸鬼'
               },
               {
                 id: 3,
-                content: '开发权限测试组件'
+                content: '进击的巨人'
               }
             ],
             doing: [
               {
-                id: 1,
-                content: '开发登录注册页面'
-              },
-              {
-                id: 2,
-                content: '开发头部组件'
-              },
-              {
-                id: 3,
-                content: '开发表格相关组件'
-              },
-              {
                 id: 4,
-                content: '开发表单相关组件'
+                content: '斩赤红之瞳'
+              },
+              {
+                id: 5,
+                content: '黑子的篮球'
+              },
+              {
+                id: 6,
+                content: '网球王子'
+              },
+              {
+                id: 7,
+                content: '通灵王'
               }
             ],
             done:[
               {
-                id: 1,
-                content: '初始化项目，生成工程目录，完成相关配置'
+                id: 8,
+                content: '灌篮高手'
               },
               {
-                id: 2,
-                content: '开发项目整体框架'
+                id: 9,
+                content: '七龙珠'
               }
             ]
           }
         },
+        components:{
+        draggable
+        }
 
     }
 </script>
@@ -97,6 +128,47 @@ h5{
       text-decoration: none;
     }
   }
+  .drag-box{
+    display: flex;
+    .drag-box-item{
+      flex: 1;
+      max-width: 330px;
+      min-width: 300px;
+      background: #eff1f5;
+      margin-right: 16px;
+      border-radius: 6px;
+      border: 1px solid #e1e4e8;
+      .item-title{
+        padding: 8px 8px 8px 12px;
+        color: #24292e;
+        font-weight: 600;
+      }
+      .item-ul{
+        padding: 0px 8px 8px;
+        height: 500px;
+        overflow-y: auto;
+        .drag-list{
+          border: 1px solid #e1e4e8;
+          padding: 10px;
+          margin: 5px 0 10px;
+          background: #fff;
+          border-radius: 6px;
+          cursor: pointer;
+          -webkit-transition: border .3s ease-in;
+          transition: border .3s ease-in;
+        }
+      }
+
+    }
+  }
+}
+.item-ul::-webkit-scrollbar{
+  width: 0;
+}
+.ghost-style{
+  display: block;
+  color: transparent;
+  border-style: dashed;
 }
 
 </style>

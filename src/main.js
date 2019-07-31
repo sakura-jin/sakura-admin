fbf1914a62f64b01bb62b5a68d655fc60bb97030
sakura <929094868@qq.com>
2019-07-31 16:59:23 +0800

    build

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
-
 Vue.use(ElementUI);
 Vue.prototype.$axios = axios;
 
